b650d13e4ba82db30e68b3e6e4f7d60772ec890e
Carlgo11 <carlgo11@users.noreply.github.com>
2021-08-04 00:49:02 +0200

    Add frontend

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM webdevops/php-nginx:7.4-alpine
+FROM webdevops/php-nginx:8.0-alpine
 COPY ./ /app
 RUN composer install -d /app
 RUN echo "variables_order = \"EGPCS\"" >> /opt/docker/etc/php/php.ini
--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php
-//TODO: Add template stuff
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new FilesystemLoader(__DIR__ . '/../templates');
+$twig = new Environment($loader, [
+    'cache' => './.compilation_cache',
+    'debug' => TRUE,
+]);
+
+$template = $twig->load('auth.twig');
+echo $template->render();
