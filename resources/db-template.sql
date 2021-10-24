ec201c153d240b2dd400bb1a64f8a0bf22023cf8
Carlgo11 <carlgo11@users.noreply.github.com>
2021-10-24 02:09:20 +0200

    Add deletion event

--- resources/db-template.sql
+++ resources/db-template.sql
@@ -2,28 +2,33 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `guest-portal` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `guest-portal` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `guest-portal`;
 
-CREATE TABLE IF NOT EXISTS `vouchers`
-(
-    `id`          char(10)             NOT NULL COMMENT 'Voucher ID',
-    `uses`        tinyint(1) UNSIGNED DEFAULT 1 COMMENT 'Voucher uses left',
-    `expiry`      int(11) UNSIGNED DEFAULT NULL COMMENT 'Voucher expiry date',
-    `duration`    int(11) UNSIGNED NOT NULL     COMMENT 'Session duration (min)',
-    `speed_limit` int(5) UNSIGNED  DEFAULT NULL COMMENT 'Transfer speed limit (MiB/s)',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-COMMIT;
+CREATE TABLE IF NOT EXISTS `vouchers` (
+                                          `id` char(10) NOT NULL COMMENT 'Voucher ID',
+                                          `uses` tinyint(1) UNSIGNED DEFAULT 1 COMMENT 'Voucher uses left',
+                                          `expiry` int(11) UNSIGNED DEFAULT NULL COMMENT 'Voucher expiry date',
+                                          `duration` int(11) UNSIGNED NOT NULL COMMENT 'Session duration (min)',
+                                          `speed_limit` int(5) UNSIGNED DEFAULT NULL COMMENT 'Transfer speed limit (MiB/s)',
+                                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 GRANT SELECT, INSERT, UPDATE (uses), DELETE ON `guest-portal`.`vouchers` TO `guest-portal`@`%`;
 
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+SET GLOBAL event_scheduler = "ON";
+
+DELIMITER $$
+CREATE DEFINER=`guest-portal`@`%` EVENT `Delete old vouchers` ON SCHEDULE EVERY 1 HOUR STARTS '2021-10-23 23:48:34' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM `vouchers` WHERE `vouchers`.`expiry` <= UNIX_TIMESTAMP(NOW())$$
+
+DELIMITER ;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
